d0ebd7a4d2daee5469559c90f07f580e16797722
Martin Miglio <marmig0404@gmail.com>
2023-04-26 04:43:26 -0400

    autofocus search bar input

--- site/src/components/EventSearchBar.tsx
+++ site/src/components/EventSearchBar.tsx
@@ -1,7 +1,7 @@
 // EventSearchBar.tsx
 /* This component is used to search for events by keywords.
 This will be used in the home page of the site to begin the flow. */
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
@@ -34,10 +34,17 @@ const EventSearchBar = ({
     setDebouncedSearchTerm("");
   };
 
+  const searchInput = useCallback((instance: HTMLInputElement | null) => {
+    if (instance) {
+      instance.focus();
+    }
+  }, []);
+
   return (
     <div className="flex justify-center items-center px-4 pb-4">
       <div className="w-full max-w-screen-sm flex items-center py-2 rounded-full bg-white">
         <input
+          ref={searchInput}
           className="appearance-none bg-transparent border-none w-full text-slate-800 py-1 px-5 leading-tight focus:outline-none"
           type="text"
           placeholder="Search for an event"
