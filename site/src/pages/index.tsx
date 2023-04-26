26976c6a67414cfa0a5815d7295fd0f7943d460b
Martin Miglio <marmig0404@gmail.com>
2023-04-26 02:09:56 -0400

    update title

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>TixTrend</title>
         <meta name="description" content="TixTrend" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
