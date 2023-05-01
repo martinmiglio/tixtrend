418b61f9f0df8ef90b2bd65020623bfc31906675
Martin Miglio <marmig0404@gmail.com>
2023-04-30 20:35:35 -0400

    add canonical url to page

--- site/src/components/page/PageHeader.tsx
+++ site/src/components/page/PageHeader.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import Script from "next/script";
 
 type PageHeaderProps = {
   title: string;
@@ -9,18 +8,20 @@ type PageHeaderProps = {
 };
 
 const PageHeader = ({ title, description, url }: PageHeaderProps) => {
+  const parsedURL = new URL(url);
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} key="ogtitle" />
       <meta property="og:description" content={description} key="ogdesc" />
-      <meta property="og:url" content={url} key="ogurl" />
+      <meta property="og:url" content={parsedURL.href} key="ogurl" />
       <meta property="og:type" content="website" />
       <meta name="twitter:site" content={title} />
       <meta name="twitter:creator" content="@martinmiglio" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
+      <link rel="canonical" href={parsedURL.protocol + "//" + parsedURL.host} />
     </Head>
   );
 };
