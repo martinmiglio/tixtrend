3de649df3bb4b50364dafc5f6a205fb64f39a361
Martin Miglio <marmig0404@gmail.com>
2023-05-02 18:54:49 -0400

    update remote fonts to use swap configure tailwind font familites

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        "Averta-Regular",
+        "Helvetica Neue",
+        "Helvetica",
+        "Arial",
+        "sans-serif",
+      ],
+      tagline: [
+        "Averta-ExtraBoldItalic",
+        "Helvetica Neue",
+        "Helvetica",
+        "Arial",
+        "sans-serif",
+      ],
+    },
   },
   plugins: [],
 };
