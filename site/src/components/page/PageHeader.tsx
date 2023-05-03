cdd58b8f9c9fc801a3ffcc05cd01fe734cbf846d
Martin Miglio <marmig0404@gmail.com>
2023-05-03 17:03:34 -0400

    add icons and images for all platforms

--- site/src/components/page/PageHeader.tsx
+++ site/src/components/page/PageHeader.tsx
@@ -17,6 +17,7 @@ const PageHeader = ({ title, description, url }: PageHeaderProps) => {
       <meta property="og:description" content={description} key="ogdesc" />
       <meta property="og:url" content={parsedURL.href} key="ogurl" />
       <meta property="og:type" content="website" />
+      <meta property="og:image" content="/wide-1200x630.png" />
       <meta name="twitter:site" content={title} />
       <meta name="twitter:creator" content="@martinmiglio" />
       <meta name="twitter:title" content={title} />
@@ -24,6 +25,27 @@ const PageHeader = ({ title, description, url }: PageHeaderProps) => {
       <link rel="canonical" href={parsedURL.protocol + "//" + parsedURL.host} />
       <link rel="icon" type="image/svg+xml" href="favicon.svg" />
       <link rel="icon" type="image/png" href="favicon.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4f46e5" />
+      <meta name="msapplication-TileColor" content="#f2f2f2" />
+      <meta name="theme-color" content="#f2f2f2" />
     </Head>
   );
 };
