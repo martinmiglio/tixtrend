4198ec9ca7c5a59d181e933e91ac524eda786153
Martin Miglio <marmig0404@gmail.com>
2023-05-04 04:28:34 -0400

    extract popup component

--- site/src/components/event/SaveEventButton.tsx
+++ site/src/components/event/SaveEventButton.tsx
@@ -11,13 +11,11 @@ import {
 
 import { event as analyticsEvent } from "@utils/analytics";
 import { EventData } from "@utils/types/EventData";
+import PopupNotification from "@components/page/PopupNotification";
 
 const SaveEventButton = ({ event }: { event: EventData }) => {
-  const popupDuration = 2000;
-
   const [saved, setSaved] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
-  const [popupOpacity, setPopupOpacity] = useState(0);
 
   useEffect(() => {
     const savedEvents = JSON.parse(localStorage.getItem("savedEvents") || "[]");
@@ -44,22 +42,17 @@ const SaveEventButton = ({ event }: { event: EventData }) => {
     setShowPopup(true);
   };
 
-  useEffect(() => {
-    if (showPopup) {
-      setPopupOpacity(1);
-      setTimeout(() => setPopupOpacity(0), 0.8 * popupDuration);
-      setTimeout(() => {
-        setShowPopup(false);
-      }, popupDuration);
-    }
-  }, [showPopup]);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    saveEvent();
+  };
 
   return (
     <>
       <button
         type="button"
         className={`btn btn-primary btn-sm mt-5 sm:mt-0`}
-        onClick={saveEvent}
+        onClick={handleClick}
       >
         <FontAwesomeIcon
           icon={saved ? faHeartCircleCheck : faHeartCirclePlus}
@@ -67,23 +60,16 @@ const SaveEventButton = ({ event }: { event: EventData }) => {
           width={25}
         />
       </button>
-      {showPopup && (
-        <div className="absolute top-0 right-0">
-          <div
-            className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 rounded-lg shadow-lg p-4"
-            style={{
-              opacity: popupOpacity,
-              transition: `opacity ${popupDuration * 0.2}ms`,
-            }}
-          >
-            <p className="text-gray-800">
-              {saved
-                ? "Event has been saved to your list!"
-                : "Event has been removed from your list!"}
-            </p>
-          </div>
-        </div>
-      )}
+      <PopupNotification
+        isActive={showPopup}
+        setIsActiveCallback={setShowPopup}
+      >
+        <p className="text-gray-800">
+          {saved
+            ? "Event has been saved to your list!"
+            : "Event has been removed from your list!"}
+        </p>
+      </PopupNotification>
     </>
   );
 };
