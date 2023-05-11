a2231fa4e6052d375cf29ee485dd1ed880e13523
Martin Miglio <marmig0404@gmail.com>
2023-05-10 21:23:44 -0400

    update html og prefix

--- site/src/pages/_document.tsx
+++ site/src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" prefix="og: https://ogp.me/ns#">
       <Head>
         <Script
           strategy="afterInteractive"
