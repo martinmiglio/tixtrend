75ca86cb981cccaface1dde1878cbe1fac929ef0
Martin Miglio <marmig0404@gmail.com>
2023-05-20 22:39:58 -0400

    add latest query to prices api

--- lambda/tixtrend-event-pricing/index.mjs
+++ lambda/tixtrend-event-pricing/index.mjs
@@ -36,40 +36,57 @@ export const handler = async (event) => {
 
 const handleGet = async (event) => {
   const { queryStringParameters } = event;
-  const { event_id } = queryStringParameters;
+  const { event_id, latest } = queryStringParameters;
 
-  if (!event_id) {
+  if (!event_id && !latest) {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: "Event ID is required.",
+        message: "Must either specify event_id or latest count.",
       }),
     };
   }
 
-  const { Items } = await dynamo
-    .query({
-      TableName: tableName,
-      KeyConditionExpression: "event_id = :event_id",
-      ExpressionAttributeValues: {
-        ":event_id": event_id,
-      },
-    })
-    .promise();
+  if (event_id) {
+    const { Items } = await dynamo
+      .query({
+        TableName: tableName,
+        KeyConditionExpression: "event_id = :event_id",
+        ExpressionAttributeValues: {
+          ":event_id": event_id,
+        },
+      })
+      .promise();
 
-  if (Items.length === 0) {
+    if (Items.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "Event not found.",
+        }),
+      };
+    }
+
     return {
-      statusCode: 404,
-      body: JSON.stringify({
-        message: "Event not found.",
-      }),
+      statusCode: 200,
+      body: JSON.stringify(Items),
     };
   }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(Items),
-  };
+  if (latest) {
+    const { Items } = await dynamo
+      .scan({
+        TableName: tableName,
+        ScanIndexForward: false,
+        Limit: parseInt(latest),
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(Items),
+    };
+  }
 };
 
 const handleOptions = async () => {
