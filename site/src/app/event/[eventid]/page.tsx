3e263bcf29b264f5c2caa094f5a170b3799f5766
Martin Miglio <marmig0404@gmail.com>
2023-08-15 16:40:06 -0400

    update data fetching in event page

--- site/src/app/event/[eventid]/page.tsx
+++ site/src/app/event/[eventid]/page.tsx
@@ -5,55 +5,69 @@ import { PriceData } from "@/utils/types/PriceData/PriceData";
 import { EventData } from "@/utils/types/EventData/EventData";
 import PriceChart from "@/components/chart/PriceChart";
 import EventInfoItem from "@/components/event/EventInfoItem";
-import LoadingDots from "@/components/page/LoadingDots";
 import BlankEventInfoItem from "@/components/event/BlankEventInfoItem";
 import Link from "next/link";
 
 const Event = ({ params }: { params: { eventid: string[] } }) => {
   const { eventid } = params;
 
-  const [loadingPriceData, setLoadingPriceData] = useState(true);
   const [eventData, setEventData] = useState<EventData | null>(null);
-  const [priceDataSet, setPriceDataSet] = useState<PriceData[]>([]);
 
-  const fetchPriceDataSet = () => {
-    fetch(`/api/get-prices?event_id=${eventid}`, {
+  const watchEvent = async () => {
+    await fetch(`/api/watch-event?event_id=${eventid}`);
+  };
+
+  const getEventData = async () => {
+    const eventDataPromise = fetch(`/api/get-event?event_id=${eventid}`, {
+      next: { revalidate: 3600 },
+    }).then((response) => {
+      if (response.status === 200) {
+        return response.json().then((data) => {
+          data.date = new Date(data.date);
+          return data;
+        });
+      } else {
+        console.warn("Something went wrong getting event data");
+        return null;
+      }
+    });
+
+    const priceHistoryPromise = fetch(`/api/get-prices?event_id=${eventid}`, {
       next: { revalidate: 43200 },
     }).then((response) => {
       if (response.status === 200) {
-        response.json().then((data) => {
+        return response.json().then((data) => {
           for (const element of data) {
             element.timestamp = new Date(element.timestamp);
           }
-          setPriceDataSet(data);
+          return data;
         });
       } else {
-        setPriceDataSet([]);
+        console.warn("Something went wrong getting price data");
+        return null;
       }
-      setLoadingPriceData(false);
     });
+
+    const [eventData, priceHistory]: [EventData | null, PriceData[] | null] =
+      await Promise.all([eventDataPromise, priceHistoryPromise]).catch(
+        (err) => {
+          console.error(err);
+          return [null, null];
+        }
+      );
+
+    if (priceHistory === null || eventData === null) {
+      return eventData;
+    }
+    return { ...eventData, priceHistory: priceHistory };
   };
 
   useEffect(() => {
     if (eventid) {
-      fetch(`/api/get-event?event_id=${eventid}`, {
-        next: { revalidate: 3600 },
-      }).then((response) => {
-        if (response.status === 200) {
-          response.json().then((data) => {
-            setEventData(data);
-          });
-        } else {
-          console.error("Something went wrong getting event");
-        }
+      getEventData().then((data) => {
+        setEventData(data);
       });
-      fetch(`/api/watch-event?event_id=${eventid}`).then((response) => {
-        if (response.status === 200) {
-          fetchPriceDataSet();
-        } else {
-          console.error("Something went wrong watching event");
-        }
-      });
+      watchEvent();
     }
   }, [eventid]);
 
@@ -71,43 +85,42 @@ const Event = ({ params }: { params: { eventid: string[] } }) => {
         <EventInfoItem eventData={eventData} showSaveButton={true} />
       </div>
       <div className="flex-1 w-screen">
-        {!loadingPriceData ? (
-          priceDataSet.length === 0 ? (
-            eventData?.date < new Date() ? (
-              <>
-                <div className="text-center text-2xl mt-10">
-                  Event has already passed.
-                </div>
-                <div className="text-center  my-10">
-                  <Link
-                    className="text-xl text-gray-400 font-thin underline hover:no-underline"
-                    href="/"
-                  >
-                    Look for other events
-                  </Link>
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="text-center text-2xl mt-10">
-                  No price history yet, check back later!
-                </div>
-                <div className="text-center text-xl my-10 text-gray-400 font-thin">
-                  Data on new events is updated once a day.
-                </div>
-              </>
-            )
-          ) : (
-            <PriceChart priceDataSet={priceDataSet} />
-          )
-        ) : (
-          <div className="flex justify-center items-center my-20">
-            <LoadingDots dotSize={8} dotSeparation={6} />
-          </div>
-        )}
+        <EventPriceChart eventData={eventData} />
       </div>
     </div>
   );
 };
 
 export default Event;
+
+const EventPriceChart = ({ eventData }: { eventData: EventData }) => {
+  if (eventData.date < new Date()) {
+    return (
+      <>
+        <div className="text-center text-2xl mt-10">
+          Event has already passed.
+        </div>
+        <div className="text-center my-10 text-xl text-gray-400 font-thin">
+          <Link className="underline hover:no-underline" href="/">
+            Look for other events
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  if (!eventData.priceHistory || eventData.priceHistory?.length === 0) {
+    return (
+      <>
+        <div className="text-center text-2xl mt-10">
+          No price history yet, check back later!
+        </div>
+        <div className="text-center text-xl my-10 text-gray-400 font-thin">
+          Data on new events is updated once a day.
+        </div>
+      </>
+    );
+  }
+
+  return <PriceChart priceDataSet={eventData.priceHistory} />;
+};
