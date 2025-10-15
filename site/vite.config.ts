deede17531d33bcd8cb6a1ce0e352aa5e59b09e1
Martin Miglio <marmig0404@gmail.com>
2025-10-15 18:06:31 -0400

    tss-migration: root site config

---
+++ site/vite.config.ts
@@ -0,0 +1,25 @@
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+    tanstackStart({
+      router: {
+        routesDirectory: "app",
+      },
+    }),
+    viteReact({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"]
+      }
+    }),
+  ],
+});
