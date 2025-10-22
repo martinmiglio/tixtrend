3446d1bfdc47c99e21888556512e692f649426f7
Martin Miglio <marmig0404@gmail.com>
2025-10-21 21:14:03 -0400

    chore: add lint config package and use it!

---
+++ packages/core/eslint.config.js
@@ -0,0 +1,8 @@
+import baseConfig from "@tixtrend/eslint-config/base";
+
+/**
+ * ESLint configuration for the core package.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export default baseConfig;
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,7 +1,7 @@
 import reactConfig from "@tixtrend/eslint-config/react";
 
 /**
- * ESLint configuration for the site app.
+ * ESLint configuration for the UI package.
  *
  * @type {import("eslint").Linter.Config[]}
  */
